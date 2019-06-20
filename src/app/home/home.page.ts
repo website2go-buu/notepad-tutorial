3facd8b81475f8db79aad4831bdb3eddd06f8c7b
VinhBuu To <vinhbuu.to@gmail.com>
2019-06-19 23:22:48 -0700

    Comlete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController, NavController } from '@ionic/angular';
+import { NotesService } from '../services/notes.service';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,38 @@ import { AlertController, NavController } from '@ionic/angular';
 })
 export class HomePage implements OnInit{
 
-  constructor(private alertCtrl: AlertController, private navCtrl: NavController) {}
+  constructor(private notesServices: NotesService, private alertCtrl: AlertController, private navCtrl: NavController) {}
 
-  ngOnInit(){}
+  ngOnInit(){
+    this.notesServices.load();
+  }
 
+
+  addNote(){
+    this.alertCtrl.create({
+      header: 'New Note',
+      message: 'What should the title of this note be?',
+      inputs: [
+        {
+          type: 'text',
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            this.notesServices.createNote(data.title);
+          }
+        }
+      ]
+    }).then(
+      (alert) => {
+        alert.present();
+      }
+    );
+  }
 }
